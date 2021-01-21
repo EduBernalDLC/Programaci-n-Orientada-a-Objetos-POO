94abfa477b7ef07771cc488cc6122bb0409e247c
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-01-21 12:42:59 -0600

    Declaración del método constructor en Java

--- Java/Car.java
+++ Java/Car.java
@@ -1,11 +1,17 @@
 class Car{
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passegenger;
+
+    public Car(String license, Account driver){//Dentro del paréntesis parametros obligatorios para crear un objeto del tipo car
+
+        this.license= license;
+        this.driver= driver;
+    }//Fin car public (Método constructor)
 
     void printDataCar(){
         System.out.println("License: " + license);
-        System.out.println("Driver: " + driver);
+        System.out.println("name Driver: " + driver.name);
     }//Fin void
 }//fin clase
--- Java/main.java
+++ Java/main.java
@@ -3,21 +3,13 @@ class main{
 
         System.out.println("Hola mundo");
 
-        Car car= new Car();
-        car.license= "AMQ123";
-        car.driver= "Andres Herrera";
+        Car car= new Car("AMQ123", new Account("Andres Herrera", "AND123"));
         car.passegenger= 4;
         car.printDataCar();
 
-        Car2 car2= new Car();
-        car2.license= "QWE567";
-        car2.driver= "Andrea Herrera";
+        Car2 car2= new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
         car2.passegenger= 3;
         car2.printDataCar();
-
-        
-        
-
 
     }//Fin main
 }//Fin clase 
