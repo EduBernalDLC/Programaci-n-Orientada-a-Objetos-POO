6c11f6542986b084e4c8fc4186b714fb03ad580a
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-01-21 13:08:07 -0600

    Método constructor en JavaScript #22

--- JS/Car.js
+++ JS/Car.js
@@ -1,6 +1,13 @@
-function Car(){
+function Car(license, driver){
     this.id;
-    this.license;
-    this.driver;
+    this.license= license;
+    this.driver= driver;
     this.passenger;
+}//Fin función
+
+Car.prototype.printDataCar= function(){
+
+    console.log(this.driver)
+    console.log(this.driver.name)
+    console.log(this.driver.document)
 }//Fin función
