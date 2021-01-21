7bb5dfa44dbe7c56fbd5c3ea81480c5fe7fd0ce4
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-01-20 20:29:20 -0600

    Inicio del proyecto

---
+++ Java/Car.java
@@ -0,0 +1,11 @@
+class Car{
+    Integer id;
+    String license;
+    String driver;
+    Integer passegenger;
+
+    void printDataCar(){
+        System.out.println("License: " + license);
+        System.out.println("Driver: " + driver);
+    }//Fin void
+}//fin clase
---
+++ Java/Payment.java
@@ -0,0 +1,3 @@
+class Payment{
+    Integer id;
+}//fin clase
