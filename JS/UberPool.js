1323ed3b8a4042832d813a997f8410d7e85123b7
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-01-23 20:43:40 -0600

    Aplicando herencia a Python y JS #27

---
+++ JS/UberPool.js
@@ -0,0 +1,10 @@
+class UberPool extends Car{
+
+    constructor(license, driver, brand, model){
+        super(license, driver)
+
+        this.brand= brand;
+        this.brand= model;
+    }//Fin constructor
+
+}//Fin clase UberPool
